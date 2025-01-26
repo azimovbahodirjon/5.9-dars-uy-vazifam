629cef747286c79a351b48e853000d88c49abba7
Bahodirjon <Bahodirjon@gmail.com>
2025-01-26 15:48:37 +0200

    Implement dark mode toggle and enhance data loading with loader visibility

--- js/app.js
+++ js/app.js
@@ -1,5 +1,266 @@
-const form = document.getElementById('form')
-const formButton = document.getElementById('form__button')
-const user = document.getElementById('user')
-const deleteBtn = document.getElementById('delete__btn')
-const clearBtn = document.getElementById('clear__button')
+const form = document.getElementById("form");
+const formButton = document.getElementById("form__button");
+const user = document.getElementById("user");
+const deleteBtn = document.getElementById("delete__btn");
+const clearBtn = document.getElementById("clear__button");
+
+// get result and update UI
+
+const updateUI = (data) => {
+  console.log("Update UI:", data);
+};
+
+/*
+ <li class="user__item">
+              <button id="delete__btn" class="user__delete--btn">
+                <i class="fas fa-trash"></i>
+              </button>
+              <img
+                class="user__img"
+                alt="User photo"
+                src="https://picsum.photos/id/338/400/400"
+                width="100"
+                height="100"
+              />
+              <div class="user__name">
+                <span class="material-symbols-outlined">badge</span>
+                <span>- Mr Akhror Soliev</span>
+              </div>
+              <div class="user__year">
+                <span class="material-symbols-outlined">cake</span>
+                <span>- 25 years old.</span>
+              </div>
+              <div class="user__location">
+                <span class="material-symbols-outlined">person_pin_circle</span>
+                <span>-Fergana, Uzbekistan</span>
+              </div>
+              <div class="user__gender">
+                <span class="material-symbols-outlined">man</span>
+                <span>- Male</span>
+              </div>
+            </li>
+            <li class="user__item">
+              <button id="delete__btn" class="user__delete--btn">
+                <i class="fas fa-trash"></i>
+              </button>
+              <img
+                class="user__img"
+                alt="User photo"
+                src="https://picsum.photos/id/338/400/400"
+                width="100"
+                height="100"
+              />
+              <div class="user__name">
+                <span class="material-symbols-outlined">badge</span>
+                <span>- Mr Akhror Soliev</span>
+              </div>
+              <div class="user__year">
+                <span class="material-symbols-outlined">cake</span>
+                <span>- 25 years old.</span>
+              </div>
+              <div class="user__location">
+                <span class="material-symbols-outlined">person_pin_circle</span>
+                <span>-Fergana, Uzbekistan</span>
+              </div>
+              <div class="user__gender">
+                <span class="material-symbols-outlined">man</span>
+                <span>- Male</span>
+              </div>
+            </li>
+            <li class="user__item">
+              <button id="delete__btn" class="user__delete--btn">
+                <i class="fas fa-trash"></i>
+              </button>
+              <img
+                class="user__img"
+                alt="User photo"
+                src="https://picsum.photos/id/338/400/400"
+                width="100"
+                height="100"
+              />
+              <div class="user__name">
+                <span class="material-symbols-outlined">badge</span>
+                <span>- Mr Akhror Soliev</span>
+              </div>
+              <div class="user__year">
+                <span class="material-symbols-outlined">cake</span>
+                <span>- 25 years old.</span>
+              </div>
+              <div class="user__location">
+                <span class="material-symbols-outlined">person_pin_circle</span>
+                <span>-Fergana, Uzbekistan</span>
+              </div>
+              <div class="user__gender">
+                <span class="material-symbols-outlined">man</span>
+                <span>- Male</span>
+              </div>
+            </li>
+            <li class="user__item">
+              <button id="delete__btn" class="user__delete--btn">
+                <i class="fas fa-trash"></i>
+              </button>
+              <img
+                class="user__img"
+                alt="User photo"
+                src="https://picsum.photos/id/338/400/400"
+                width="100"
+                height="100"
+              />
+              <div class="user__name">
+                <span class="material-symbols-outlined">badge</span>
+                <span>- Mr Akhror Soliev</span>
+              </div>
+              <div class="user__year">
+                <span class="material-symbols-outlined">cake</span>
+                <span>- 25 years old.</span>
+              </div>
+              <div class="user__location">
+                <span class="material-symbols-outlined">person_pin_circle</span>
+                <span>-Fergana, Uzbekistan</span>
+              </div>
+              <div class="user__gender">
+                <span class="material-symbols-outlined">man</span>
+                <span>- Male</span>
+              </div>
+            </li>
+            <li class="user__item">
+              <button id="delete__btn" class="user__delete--btn">
+                <i class="fas fa-trash"></i>
+              </button>
+              <img
+                class="user__img"
+                alt="User photo"
+                src="https://picsum.photos/id/338/400/400"
+                width="100"
+                height="100"
+              />
+              <div class="user__name">
+                <span class="material-symbols-outlined">badge</span>
+                <span>- Mr Akhror Soliev</span>
+              </div>
+              <div class="user__year">
+                <span class="material-symbols-outlined">cake</span>
+                <span>- 25 years old.</span>
+              </div>
+              <div class="user__location">
+                <span class="material-symbols-outlined">person_pin_circle</span>
+                <span>-Fergana, Uzbekistan</span>
+              </div>
+              <div class="user__gender">
+                <span class="material-symbols-outlined">man</span>
+                <span>- Male</span>
+              </div>
+            </li>
+            <li class="user__item">
+              <button id="delete__btn" class="user__delete--btn">
+                <i class="fas fa-trash"></i>
+              </button>
+              <img
+                class="user__img"
+                alt="User photo"
+                src="https://picsum.photos/id/338/400/400"
+                width="100"
+                height="100"
+              />
+              <div class="user__name">
+                <span class="material-symbols-outlined">badge</span>
+                <span>- Mr Akhror Soliev</span>
+              </div>
+              <div class="user__year">
+                <span class="material-symbols-outlined">cake</span>
+                <span>- 25 years old.</span>
+              </div>
+              <div class="user__location">
+                <span class="material-symbols-outlined">person_pin_circle</span>
+                <span>-Fergana, Uzbekistan</span>
+              </div>
+              <div class="user__gender">
+                <span class="material-symbols-outlined">man</span>
+                <span>- Male</span>
+              </div>
+            </li>
+            <li class="user__item">
+              <button id="delete__btn" class="user__delete--btn">
+                <i class="fas fa-trash"></i>
+              </button>
+              <img
+                class="user__img"
+                alt="User photo"
+                src="https://picsum.photos/id/338/400/400"
+                width="100"
+                height="100"
+              />
+              <div class="user__name">
+                <span class="material-symbols-outlined">badge</span>
+                <span>- Mr Akhror Soliev</span>
+              </div>
+              <div class="user__year">
+                <span class="material-symbols-outlined">cake</span>
+                <span>- 25 years old.</span>
+              </div>
+              <div class="user__location">
+                <span class="material-symbols-outlined">person_pin_circle</span>
+                <span>-Fergana, Uzbekistan</span>
+              </div>
+              <div class="user__gender">
+                <span class="material-symbols-outlined">man</span>
+                <span>- Male</span>
+              </div>
+            </li>
+            <li class="user__item">
+              <button id="delete__btn" class="user__delete--btn">
+                <i class="fas fa-trash"></i>
+              </button>
+              <img
+                class="user__img"
+                alt="User photo"
+                src="https://picsum.photos/id/338/400/400"
+                width="100"
+                height="100"
+              />
+              <div class="user__name">
+                <span class="material-symbols-outlined">badge</span>
+                <span>- Mr Akhror Soliev</span>
+              </div>
+              <div class="user__year">
+                <span class="material-symbols-outlined">cake</span>
+                <span>- 25 years old.</span>
+              </div>
+              <div class="user__location">
+                <span class="material-symbols-outlined">person_pin_circle</span>
+                <span>-Fergana, Uzbekistan</span>
+              </div>
+              <div class="user__gender">
+                <span class="material-symbols-outlined">man</span>
+                <span>- Male</span>
+              </div>
+            </li>
+            <li class="user__item">
+              <button id="delete__btn" class="user__delete--btn">
+                <i class="fas fa-trash"></i>
+              </button>
+              <img
+                class="user__img"
+                alt="User photo"
+                src="https://picsum.photos/id/338/400/400"
+                width="100"
+                height="100"
+              />
+              <div class="user__name">
+                <span class="material-symbols-outlined">badge</span>
+                <span>- Mr Akhror Soliev</span>
+              </div>
+              <div class="user__year">
+                <span class="material-symbols-outlined">cake</span>
+                <span>- 25 years old.</span>
+              </div>
+              <div class="user__location">
+                <span class="material-symbols-outlined">person_pin_circle</span>
+                <span>-Fergana, Uzbekistan</span>
+              </div>
+              <div class="user__gender">
+                <span class="material-symbols-outlined">man</span>
+                <span>- Male</span>
+              </div>
+            </li>
+*/
--- js/request.js
+++ js/request.js
@@ -1,5 +1,51 @@
 // api
-const API = 'https://randomuser.me/api/?results=9'
+const API = "https://randomuser.me/api/?results=9";
 
 // for leader
-const overlay = document.getElementById('overlay')
+const overlay = document.getElementById("overlay");
+
+// toggle Loader
+const loaderToggle = (toggle) => {
+  if (toggle) {
+    overlay.classList.remove("hidden");
+  } else {
+    overlay.classList.add("hidden");
+  }
+};
+
+// reques promise
+
+const getData = (resource) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState < 4) {
+        loaderToggle(true);
+      } else if (request.readyState == 4 && request.status == 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data.results);
+        loaderToggle(false);
+      } else if (request.readyState == 4) {
+        reject("Error !!!");
+        loaderToggle(false);
+      }
+    });
+
+    request.open("GET", resource);
+    request.send();
+  });
+};
+
+// Load
+const reload = () => {
+  getData(API)
+    .then((data) => {
+      updateUI("then data:", data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+document.addEventListener("DOMContentLoaded", reload);
